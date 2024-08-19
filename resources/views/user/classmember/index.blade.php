4ad92df9575bbae7039023c215f8c07e1cd99de8
Karishma271 <karishmapatel273@gmail.com>
2024-08-19 13:34:22 -0400

    update

--- resources/views/user/classmember/index.blade.php
+++ resources/views/user/classmember/index.blade.php
@@ -2,52 +2,39 @@
 
 @section('content')
 <div class="container mt-4">
-    <h1>Assigned Class Members</h1>
+    <h1 class="text-center">Assigned Class Members</h1>
 
     @if($classMembers->isEmpty())
-        <p>No assigned members found.</p>
+        <p class="text-center">No assigned members found.</p>
     @else
-        <div class="table-responsive">
-            <table class="table table-striped table-hover">
-                <thead class="table-primary">
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Image</th>
-                        <th>Class Name</th>
-                        <th>Class Time</th>
-                        <th>Class Video</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($classMembers as $member)
-                    <tr>
-                        <td>{{ $member->name }}</td>
-                        <td>{{ $member->email }}</td>
-                        <td>{{ ucfirst($member->role) }}</td>
-                        <td>
-                            @if($member->image && file_exists(public_path('images/instructors/' . $member->image)))
-                                <img src="{{ asset('images/instructors/' . $member->image) }}" alt="Member Image" class="img-thumbnail" style="width:100px;height:100px;">
-                            @else
-                                <img src="{{ asset('images/instructors/no-image.png') }}" alt="No Image" class="img-thumbnail" style="width:100px;height:100px;">
-                            @endif
-                        </td>
-                        <td>{{ $member->class_name ?? 'No Class' }}</td>
-                        <td>{{ $member->class_time ? date('d-m-Y H:i', strtotime($member->class_time)) : 'No Time' }}</td>
-                        <td>
-                        @if($member->video_id)
-                            <div class="ratio ratio-16x9" style="max-width: 200px;">
-                                <iframe src="https://www.youtube.com/embed/{{ $member->video_id }}" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+        <div class="row">
+            @foreach($classMembers as $member)
+                <div class="col-md-4 mb-4">
+                    <div class="card shadow-sm h-100 border-0">
+                        <div class="card-body text-center">
+                            <div class="avatar mb-4">
+                                @if($member->image && file_exists(public_path('images/instructors/' . $member->image)))
+                                    <img src="{{ asset('images/instructors/' . $member->image) }}" alt="Member Image" class="img-fluid rounded-circle">
+                                @else
+                                    <img src="{{ asset('images/instructors/no-image.png') }}" alt="No Image" class="img-fluid rounded-circle">
+                                @endif
                             </div>
-                        @else
-                            <span class="text-muted">No Video</span>
-                        @endif
-                    </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                            <h5 class="card-title font-weight-bold">{{ $member->name }}</h5>
+                            <p class="card-text text-muted">{{ $member->email }}</p>
+                            <p class="card-text"><strong>Role:</strong> {{ ucfirst($member->role) }}</p>
+                            <p class="card-text"><strong>Class:</strong> {{ $member->class_name ?? 'No Class Assigned' }}</p>
+                            <p class="card-text"><strong>Class Time:</strong> {{ $member->class_time ? \Carbon\Carbon::parse($member->class_time)->format('d-m-Y H:i') : 'No Time Available' }}</p>
+                            @if($member->video_id)
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $member->video_id }}" allowfullscreen></iframe>
+                                </div>
+                            @else
+                                <p class="text-muted">No Video Available</p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     @endif
 </div>
